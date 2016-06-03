d0b1fac811e052aa2f1e50f4bafda8252c0a4128
Plyom Developer <plyomdev@Plyoms-iMac.local>
2016-06-03 16:05:13 -0300

    Improve design

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 
+$(document).ready(function() {
+    $(".top-navigation").hide();
+  }
+);
+$(window).scroll(function (event) {
+  var scroll = $(window).scrollTop();
+  var distanceWithoutHeader = $(window).height()*0.3;
+  if (scroll > distanceWithoutHeader) {
+    $(".top-navigation").show();
+  } else {
+    $(".top-navigation").hide();
+  }
+});
