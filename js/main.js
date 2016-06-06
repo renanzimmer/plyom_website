ec72563dfb8170c7f00b04277536e53c928713e6
Plyom Developer <plyomdev@Plyoms-iMac.local>
2016-06-06 17:20:32 -0300

    Finish animations

--- js/main.js
+++ js/main.js
@@ -1,16 +1,41 @@
 
 $(document).ready(function() {
-    $(".top-navigation").hide();
+    changeNavFromScrollPosition($(window).scrollTop(), $(window).height());
   }
 );
 $(window).scroll(function (event) {
-  var scroll = $(window).scrollTop();
-  var distanceWithoutHeader = $(window).height()*0.3;
+  event.preventDefault();
+  changeNavFromScrollPosition($(window).scrollTop(), $(window).height());
+});
+
+function changeNavFromScrollPosition(scroll, windowHeight) {
+  var distanceWithoutHeader = windowHeight*0.3;
   if (scroll > distanceWithoutHeader) {
     $(".top-navigation").show();
   } else {
     $(".top-navigation").hide();
   }
+  if (scroll >= windowHeight) {
+    $(".menu").find(".active").removeClass("active");
+    $(".our-cases-nav").addClass("active");
+  }
+  if (scroll >= (windowHeight + windowHeight*0.8)) {
+    $(".menu").find(".active").removeClass("active");
+    $(".services-nav").addClass("active");
+  }
+  if (scroll >= (windowHeight + 2*(windowHeight*0.8))) {
+    $(".menu").find(".active").removeClass("active");
+    $(".methods-nav").addClass("active");
+  }
+  if (scroll >= (windowHeight + 3*(windowHeight*0.7))) {
+    $(".menu").find(".active").removeClass("active");
+    $(".contact-nav").addClass("active");
+  }
+}
+
+$(".nav-item").on("click", function(e) {
+  e.preventDefault();
+  changeCase($(this));
 });
 
 function changeCase(selector) {
@@ -23,7 +48,27 @@ function changeCase(selector) {
   $(selector).closest(".pagination").find(".active").removeClass("active");
   $("." + section + "-nav-item").addClass("active");
 }
-$(".nav-item").on("click", function(e) {
+
+function scrollPage(distanceFromTop){
+  $(window).scrollTop(distanceFromTop);
+}
+$(".nav-menu-item").on("click", function(e) {
   e.preventDefault();
-  changeCase($(this));
+  var itemClass = $(this).attr("class").split(" ")[0];
+  var windowHeight = $(window).height();
+  if (itemClass == "our-cases-nav") {
+    scrollPage(windowHeight);
+  }
+  if (itemClass == "services-nav") {
+    scrollPage(windowHeight + windowHeight*0.8);
+  }
+  if (itemClass == "methods-nav") {
+    scrollPage(windowHeight + 2*(windowHeight*0.8));
+  }
+  if (itemClass == "contact-nav") {
+    scrollPage(windowHeight + 3*(windowHeight*0.8));
+  }
+  $(this).closest(".menu").find(".active").removeClass("active");
+  $(this).addClass("active");
+
 });
