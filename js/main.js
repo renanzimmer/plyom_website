7e44e0feebb0aae663df3a47815b5aa8abd7752b
Plyom Developer <plyomdev@Plyoms-iMac.local>
2016-06-06 14:20:16 -0300

    Finish website without correct content, alignments and some animations

--- js/main.js
+++ js/main.js
@@ -12,3 +12,18 @@ $(window).scroll(function (event) {
     $(".top-navigation").hide();
   }
 });
+
+function changeCase(selector) {
+  $(".minsulin-case").hide();
+  $(".ahgora-case").hide();
+  $(".youper-case").hide();
+  $(".anestech-case").hide();
+  var section = ($(selector).attr("class").split(" ")[0].split("-")[0]);
+  $("." + section + "-case").show();
+  $(selector).closest(".pagination").find(".active").removeClass("active");
+  $("." + section + "-nav-item").addClass("active");
+}
+$(".nav-item").on("click", function(e) {
+  e.preventDefault();
+  changeCase($(this));
+});
